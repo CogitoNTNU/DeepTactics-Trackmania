1690dde5cb9096e042b62563839dc3abd2c236e0
EldarAlvik <55811105+EldarAlvik@users.noreply.github.com>
2025-10-29 03:08:17 +0100

    feat: added env logic for import of training model

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-from src.env_tm import run_training
-# from src.env import run_training
+from config_files.tm_config import Config
+
+config = Config
+match config.env_name:
+    case "CarRacing-v3":
+        from src.env_tm import run_training 
+    case "LunarLander-v3":
+        from src.env import run_training
+    case "CartPole-v1":
+        from src.env import run_training
+    case "TM20":
+        from src.env_tm import run_training
+
+
 
 if __name__ == "__main__":
     run_training()
